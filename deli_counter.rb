2c9b6c8677a27af2525685cb4e857532975e446a
Matteo Piccini <gnappo.1@live.it>
2019-10-10 02:44:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 # Write your code here.
 def line(people)
   if people.size == 0
-   puts "The line is currently empty."
+    puts "The line is currently empty."
   else
-    sentence = "The line is currently: "
-    people.each.with_index(1) {|person, i| sentence << "#{i}. #{person}"}
+    sentence = "The line is currently:"
+    people.each.with_index(1) {|person, i| sentence << " #{i}. #{person}"}
     puts sentence
   end
 end
