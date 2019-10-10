204390ed0d1102b3a949a619d7799fd82362db5e
Matteo Piccini <gnappo.1@live.it>
2019-10-10 02:52:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,5 +15,5 @@ def take_a_number(line, person)
 end
 
 def now_serving(line)
-  line.empty? ? puts "There is nobody waiting to be served!" : 
+  line.empty? ? puts "There is nobody waiting to be served!" : puts "Currently serving #{line.shift}."
 end
