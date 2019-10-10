f0ca96ce3f9c6b10bedfa207bfe9a93c4dadd159
Matteo Piccini <gnappo.1@live.it>
2019-10-10 02:39:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,10 @@
 # Write your code here.
 def line(people)
-  
+  if people.empty? 
+    "The line is currently empty."
+  else
+    sentence = "The line is currently: "
+    people.each.with_index(1) {|person, i| sentence << "#{i}. #{person}"
+    sentence
+  end
 end
